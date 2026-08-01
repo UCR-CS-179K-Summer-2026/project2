a8173f1f1d97d8e5ea4d83388cf4d5b6957d4550
AaronM1-1 <matasaaron80@gmail.com>
2026-07-31 17:48:28 -0700

    string detection

--- parser.cpp
+++ parser.cpp
@@ -20,5 +20,40 @@ int main() {
 
     jsonData.resize(size);
     file.read(jsonData.data(), size);
+    if (!file) {
+        std::cout << "Incomplete/Failed Read \n";
+        return 1;
+    }
 
+    bool inString = false;
+    int counter = 0;
+    
+    for(int i = 0; i < jsonData.size(); i++) {
+        
+        if(inString == false) {
+            if(jsonData.at(i) == '"') {
+                inString = true;
+            }
+        }
+        else {
+            if(jsonData.at(i) == '\\') {
+                counter++;
+            }
+            else if(jsonData.at(i) == '"' && counter >= 1) {
+                if(counter % 2 == 0) {
+                    
+                    inString = false;
+                    counter = 0;
+                }
+                else {
+                    
+                    counter = 0;
+                }
+            }
+            else {
+                counter = 0;
+            }
+        }
+    }
 }
+
